9f0fe1488411878c5eb403e2db769d0d5f9ea13f
SharmaPrateek18 <126266854+SharmaPrateek18@users.noreply.github.com>
2023-02-27 01:33:06 +0530

    Update Array_Multiplier_tb.v

--- Array_Multiplier_tb.v
+++ Array_Multiplier_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2023 08:53:57 PM
-// Design Name: 
-// Module Name: Array_Multiplier_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module Array_Multiplier_tb;

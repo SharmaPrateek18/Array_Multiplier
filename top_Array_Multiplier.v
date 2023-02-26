0d9a4dc4232c11584ed1699df809e2d5ede8ae5c
SharmaPrateek18 <126266854+SharmaPrateek18@users.noreply.github.com>
2023-02-27 01:31:41 +0530

    Update top_Array_Multiplier.v

--- top_Array_Multiplier.v
+++ top_Array_Multiplier.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2023 08:48:23 PM
-// Design Name: 
-// Module Name: top_Array_Multiplier
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module top_Array_Multiplier(a,b,out);
